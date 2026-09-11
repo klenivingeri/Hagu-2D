0ccbadada4066e00134b4426e43a9aef99027a40
klenivingeri <libertti@live.com>
2026-09-11 13:42:12 -0300

    chore: map

--- src/game/config/maps.js
+++ src/game/config/maps.js
@@ -100,9 +100,9 @@ export const MAPS = {
 // linha = "down", andar uma coluna = "left"/"right". map_0 é o hub central;
 // gelo/fogo esticam pra cima/baixo, floresta/deserto esticam pros lados.
 export const MAP_GRID = [
-  ['map_forest_4',           null,           'map_ice_2',    null,            null],
-  ['map_forest_3',           null,           'map_ice_1',    null,            null],
-  ['map_forest_2', 'map_forest_1', 'map_0',        'map_desert_1',  'map_desert_2'],
+  [null,           null,           'map_ice_2',    null,            'map_forest_4'],
+  [null,           null,           'map_ice_1',    null,            'map_forest_3'],
+  ['map_desert_2', 'map_desert_1', 'map_0',        'map_forest_1',  'map_forest_2'],
   [null,           null,           'map_fire_1',   null,            null],
   [null,           null,           'map_fire_2',   null,            null],
 ];
